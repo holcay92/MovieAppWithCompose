3d2c1a06e2138d2230760ec54f3b58598f52ed09
H.Halil Olcay <holcay@marun.edu.tr>
2023-08-16 09:55:24 +0300

    lazy column instead of recycler view example

--- ComposeTutorial1/app/src/main/java/com/example/composetutorial1/MainActivity.kt
+++ ComposeTutorial1/app/src/main/java/com/example/composetutorial1/MainActivity.kt
@@ -3,19 +3,16 @@ package com.example.composetutorial1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -41,39 +37,40 @@ data class Message(val author: String, val body: String)
 @Composable
 fun MessageCard(msg: Message) {
     // Add padding around our message
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(all = 16.dp).background(Color.White)
-            .border(1.dp, Color.Red).fillMaxSize(),
-
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.background(Color.LightGray).fillMaxSize().padding(5.dp)
     ) {
-        Image(
-            painter = painterResource(R.drawable.image1),
-            contentDescription = "Contact profile picture",
+        items(20) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .size(40.dp),
-        )
-
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(10.dp))
-
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("hello ${msg.author}!")
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(msg.body)
-        }
-        Box {
-            Text(
-                "Box",
-                modifier = Modifier.width(50.dp).height(30.dp).background(Color.Red)
-                    .border(1.dp, Color.Black),
-            )
-            Text(
-                "Big Box",
-                modifier = Modifier.width(100.dp).background(Color.Green).clip(shape = CircleShape),
-            )
+                    .border(1.dp, Color.Black),) {
+                Text("hello ${msg.author}!")
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(msg.body)
+            }
+            Box {
+                Text(
+                    "Box",
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(30.dp)
+                        .background(Color.Red)
+                        .border(1.dp, Color.Black)
+                        .align(Alignment.BottomEnd),
+                )
+                Text(
+                    "Big Box",
+                    modifier = Modifier
+                        .width(100.dp)
+                        .background(Color.Green)
+                        .clip(shape = CircleShape)
+                        .align(
+                            Alignment.TopStart,
+                        ),
+                )
+            }
         }
     }
 }
